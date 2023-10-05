487d01aa680fb89f304cf0b3093c9bd0c86f639b
Toru Kobayashi <koba0004@gmail.com>
2023-10-05 23:45:09 +0900

    fix: do not cache database access

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import Image from 'next/image'
 import { PrismaClient } from '@prisma/client'
 
+export const dynamic = 'force-dynamic'
+
 const prisma = new PrismaClient()
 
 export default async function Home() {
